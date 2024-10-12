721973721a5387312ea9a722fda98c8664760d8f
Bruno Campos <brunosopmac@gmail.com>
2024-10-12 20:15:12 -0300

    Navegação + Validação

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -17,7 +17,11 @@ import { formatPrice } from '../../utils/formatPrice'
 import { RootReducer } from '../../store'
 import { useState } from 'react'
 
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
 const Cart = () => {
+  const [isPaymentStep, setIsPaymentStep] = useState(false)
   const [showCartStepForm, setShowCartStepForm] = useState('cart')
   const { isOpen, items } = useSelector((state: RootReducer) => state.cart)
   const dispatch = useDispatch()
@@ -35,6 +39,79 @@ const Cart = () => {
     return items.reduce((acc, curr) => {
       return (acc += curr.preco)
     }, 0)
+  }
+
+  const form = useFormik({
+    initialValues: {
+      receiver: '',
+      address: '',
+      city: '',
+      cep: '',
+      number: '',
+      complement: '',
+      cardOwner: '',
+      cardNumber: '',
+      cardCode: '',
+      dueMonth: '',
+      dueYear: ''
+    },
+    validationSchema: Yup.object({
+      receiver: Yup.string()
+        .min(5, 'O nome precisa ter pelo menos 5 caracteres')
+        .required('O campo é obrigatório'),
+      address: Yup.string().required('O campo é obrigatório'),
+      city: Yup.string().required('O campo é obrigatório'),
+      cep: Yup.string()
+        .min(9, 'O nome precisa ter pelo menos 9 caracteres')
+        .max(9, 'O nome precisa ter pelo menos 9 caracteres')
+        .required('O campo é obrigatório'),
+      number: Yup.number().required('O campo é obrigatório'),
+      cardOwner: Yup.string().when((values, schema) =>
+        isPaymentStep
+          ? schema.required('Nome do titular é obrigatório')
+          : schema
+      ),
+      cardNumber: Yup.string().when((values, schema) =>
+        isPaymentStep
+          ? schema.required('Número do cartão é obrigatório')
+          : schema
+      ),
+      cardCode: Yup.number().when((values, schema) =>
+        isPaymentStep
+          ? schema.min(3, 'Minimo 3 caracteres').required('CVV é obrigatório')
+          : schema
+      ),
+      dueMonth: Yup.string().when((values, schema) =>
+        isPaymentStep ? schema.required('Mês é obrigatório') : schema
+      ),
+      dueYear: Yup.string().when((values, schema) =>
+        isPaymentStep ? schema.required('Ano é obrigatório') : schema
+      )
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
+  const validatePayment = () => {
+    form.handleSubmit()
+
+    if (form.isValid) {
+      setShowCartStepForm('payment')
+      setIsPaymentStep(true)
+    }
+  }
+
+  const submitForm = () => {
+    form.handleSubmit()
+  }
+
+  const getErrorMessage = (fieldName: string, message?: string) => {
+    const isTouched = fieldName in form.touched
+    const isInvalid = fieldName in form.errors
+
+    if (isTouched && isInvalid) return message
+    return ''
   }
 
   return (
@@ -64,89 +141,205 @@ const Cart = () => {
           </>
         )}
 
-        {showCartStepForm === 'delivery' && (
-          <>
-            <form>
+        <form onSubmit={form.handleSubmit}>
+          {showCartStepForm === 'delivery' && (
+            <>
               <Card>
                 <h3>Entrega</h3>
                 <Row>
                   <InputGroup>
-                    <label htmlFor="">Quem irá receber</label>
-                    <input id="" type="text" name="" />
+                    <label htmlFor="receiver">Quem irá receber</label>
+                    <input
+                      type="text"
+                      id="receiver"
+                      name="receiver"
+                      onBlur={form.handleBlur}
+                      value={form.values.receiver}
+                      onChange={form.handleChange}
+                    />
+                    <small>
+                      {getErrorMessage('receiver', form.errors.receiver)}
+                    </small>
                   </InputGroup>
                   <InputGroup>
-                    <label htmlFor="">Endereço</label>
-                    <input id="" type="text" name="" />
+                    <label htmlFor="address">Endereço</label>
+                    <input
+                      type="text"
+                      id="address"
+                      name="address"
+                      onBlur={form.handleBlur}
+                      value={form.values.address}
+                      onChange={form.handleChange}
+                    />
+                    <small>
+                      {getErrorMessage('address', form.errors.address)}
+                    </small>
                   </InputGroup>
                   <InputGroup>
-                    <label htmlFor="">Cidade</label>
-                    <input id="" type="text" name="" />
+                    <label htmlFor="city">Cidade</label>
+                    <input
+                      type="text"
+                      id="city"
+                      name="city"
+                      onBlur={form.handleBlur}
+                      value={form.values.city}
+                      onChange={form.handleChange}
+                    />
+                    <small>{getErrorMessage('city', form.errors.city)}</small>
                   </InputGroup>
                   <InputGroup size="50">
-                    <label htmlFor="">CEP</label>
-                    <input id="" type="text" name="" />
+                    <label htmlFor="cep">CEP</label>
+                    <input
+                      type="text"
+                      id="cep"
+                      name="cep"
+                      onBlur={form.handleBlur}
+                      value={form.values.cep}
+                      onChange={form.handleChange}
+                    />
+                    <small>{getErrorMessage('cep', form.errors.cep)}</small>
                   </InputGroup>
                   <InputGroup size="50">
-                    <label htmlFor="">Número</label>
-                    <input id="" type="text" name="" />
+                    <label htmlFor="number">Número</label>
+                    <input
+                      type="text"
+                      id="number"
+                      name="number"
+                      onBlur={form.handleBlur}
+                      value={form.values.number}
+                      onChange={form.handleChange}
+                    />
+                    <small>
+                      {getErrorMessage('number', form.errors.number)}
+                    </small>
                   </InputGroup>
                   <InputGroup>
-                    <label htmlFor="">Complemento (opcional)</label>
-                    <input id="" type="text" name="" />
+                    <label htmlFor="complement">Complemento (opcional)</label>
+                    <input
+                      type="text"
+                      id="complement"
+                      name="complement"
+                      onBlur={form.handleBlur}
+                      value={form.values.complement}
+                      onChange={form.handleChange}
+                    />
+                    <small>
+                      {getErrorMessage('complement', form.errors.complement)}
+                    </small>
                   </InputGroup>
                 </Row>
               </Card>
               <Action>
-                <Button onClick={() => setShowCartStepForm('payment')}>
+                <Button
+                  type="button"
+                  disabled={!form.dirty}
+                  onClick={() => validatePayment()}
+                >
                   Continuar com o pagamento
                 </Button>
                 <Button onClick={() => setShowCartStepForm('cart')}>
                   Voltar para o carrinho
                 </Button>
               </Action>
-            </form>
-          </>
-        )}
+            </>
+          )}
 
-        {showCartStepForm === 'payment' && (
-          <>
-            <form>
+          {showCartStepForm === 'payment' && (
+            <>
               <Card>
                 <h3>
                   Pagamento - Valor a pagar {formatPrice(getTotalPrice())}
                 </h3>
                 <Row>
                   <InputGroup>
-                    <label htmlFor="">Nome no cartão</label>
-                    <input id="" type="text" name="" />
+                    <label htmlFor="cardOwner">Nome no cartão</label>
+                    <input
+                      type="text"
+                      id="cardOwner"
+                      name="cardOwner"
+                      onBlur={form.handleBlur}
+                      value={form.values.cardOwner}
+                      onChange={form.handleChange}
+                    />
+                    <small>
+                      {getErrorMessage('cardOwner', form.errors.cardOwner)}
+                    </small>
                   </InputGroup>
                   <InputGroup>
-                    <label htmlFor="70">Número do cartão</label>
-                    <input id="" type="text" name="" />
+                    <label htmlFor="cardNumber">Número do cartão</label>
+                    <input
+                      type="text"
+                      id="cardNumber"
+                      name="cardNumber"
+                      onBlur={form.handleBlur}
+                      value={form.values.cardNumber}
+                      onChange={form.handleChange}
+                    />
+                    <small>
+                      {getErrorMessage('cardNumber', form.errors.cardNumber)}
+                    </small>
                   </InputGroup>
                   <InputGroup size="30">
-                    <label htmlFor="">CVV</label>
-                    <input id="" type="text" name="" />
+                    <label htmlFor="cardCode">CVV</label>
+                    <input
+                      type="number"
+                      id="cardCode"
+                      maxLength={3}
+                      name="cardCode"
+                      onBlur={form.handleBlur}
+                      value={form.values.cardCode}
+                      onChange={form.handleChange}
+                    />
+                    <small>
+                      {getErrorMessage('cardCode', form.errors.cardCode)}
+                    </small>
                   </InputGroup>
                   <InputGroup size="50">
-                    <label htmlFor="">Mês de vencimento</label>
-                    <input id="" type="text" name="" />
+                    <label htmlFor="dueMonth">Mês de vencimento</label>
+                    <input
+                      type="text"
+                      id="dueMonth"
+                      name="dueMonth"
+                      onBlur={form.handleBlur}
+                      value={form.values.dueMonth}
+                      onChange={form.handleChange}
+                    />
+                    <small>
+                      {getErrorMessage('dueMonth', form.errors.dueMonth)}
+                    </small>
                   </InputGroup>
                   <InputGroup size="50">
-                    <label htmlFor="">Ano de vencimento</label>
-                    <input id="" type="text" name="" />
+                    <label htmlFor="dueYear">Ano de vencimento</label>
+                    <input
+                      type="text"
+                      id="dueYear"
+                      name="dueYear"
+                      onBlur={form.handleBlur}
+                      value={form.values.dueYear}
+                      onChange={form.handleChange}
+                    />
+                    <small>
+                      {getErrorMessage('dueYear', form.errors.dueYear)}
+                    </small>
                   </InputGroup>
                 </Row>
               </Card>
               <Action>
-                <Button>Finalizar pagamento</Button>
-                <Button onClick={() => setShowCartStepForm('delivery')}>
+                <Button type="submit" onClick={() => submitForm()}>
+                  Finalizar pagamento
+                </Button>
+                <Button
+                  onClick={() => {
+                    setShowCartStepForm('delivery')
+                    setIsPaymentStep(false)
+                  }}
+                >
                   Voltar para edição de endereço
                 </Button>
               </Action>
-            </form>
-          </>
-        )}
+            </>
+          )}
+        </form>
       </Sidebar>
     </CartContainer>
   )
